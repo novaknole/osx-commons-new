7dc67634a9315b55753c072ce34a3a7a97ed095e
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2024-11-28 16:55:11 +0400

    change3-contracts

--- contracts/src/plugin/IPlugin.sol
+++ contracts/src/plugin/IPlugin.sol
@@ -20,7 +20,8 @@ interface IPlugin {
         enumNine,
         enum1000,
         sdasdasd,
-        oasdkasd
+        oasdkasd,
+        kkkkda
     }
 
     /// @notice Types of plugin implementations available within OSx.
